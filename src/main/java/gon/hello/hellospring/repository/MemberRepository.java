be6b7625e82a71c04aa0747a835ce6337342070b
GON <gon.song@balancehero.com>
2022-10-01 20:15:48 +0900

    memory repository Test

---
+++ src/main/java/gon/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,18 @@
+package gon.hello.hellospring.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import gon.hello.hellospring.domain.Member;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+
+    Optional<Member> findById(long id);         // 리턴이 Null일 수도 있어서
+
+    Optional<Member> findByName(String name);
+
+    List<Member> findAll();
+
+}

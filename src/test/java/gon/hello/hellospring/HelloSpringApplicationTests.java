9709249b1fd18275f19658dbe241e7659df8c09b
GON <gon.song@balancehero.com>
2022-10-01 14:54:58 +0900

    initial commit

---
+++ src/test/java/gon/hello/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package gon.hello.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
